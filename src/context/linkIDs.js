35bd2e8a8d887db85ed22945dd51af9c45bbbb1b
rollinstock <tgutierrez2002@gmail.com>
2019-07-29 21:31:19 -0700

    add links

---
+++ src/context/linkIDs.js
@@ -0,0 +1,34 @@
+export const GlazingCFR = "http://www.ecfr.gov/cgi-bin/retrieveECFR?gp=&SID=c4bd7d71907e70fdd72f2235963ec7a6&mc=true&n=pt49.4.223&r=PART&ty=HTML#ap49.4.223_117.a"
+export const Seats_FST_cfr238_103AppendixB = "http://www.ecfr.gov/cgi-bin/retrieveECFR?gp=&SID=2ef853b3d91aaa868fd32779e538f4a1&mc=true&n=pt49.4.238&r=PART&ty=HTML#ap49.4.238_1603.b"***testLink
+export const Seats_FST_cfr238_103AppendixB = "http://www.ecfr.gov/cgi-bin/retrieveECFR?gp=&SID=2ef853b3d91aaa868fd32779e538f4a1&mc=true&n=pt49.4.238&r=PART&ty=HTML#ap49.4.238_1603.b"***testLink
+export const Seats_CalTB133 = "http://www.bearhfti.ca.gov/industry/tb133.pdf"***testLink
+export const Seats_strength_238_233a = "http://www.ecfr.gov/cgi-bin/text-idx?SID=fd8c8579ed8590671f4da10d75b7f8b9&mc=true&node=se49.4.238_1233&rgn=div8"***testLink
+export const Seats_APTA = "http://www.apta.com/resources/standards/Documents/APTA-PR-CS-S-016-99.pdf"
+export const Seats_designguidelines = "http://downloads.transportation.org/NextGenEquipmentComm/C21/Caltrans%20C21%20Specification/Specification%209-101%20Seats.rev%20D.1.pdf"
+export const Seats_designguidelines = "http://downloads.transportation.org/NextGenEquipmentComm/C21/Caltrans%20C21%20Specification/Specification%209-101%20Seats.rev%20D.1.pdf"
+export const APTA_Interior_fittings = "http://www.apta.com/resources/standards/Documents/APTA-PR-CS-S-006-98.pdf");
+export const ADA_interior_circulation_StanchionsHandrails = "http://www.apta.com/resources/standards/Documents/APTA-PR-CS-S-006-98.pdf");
+export const APTA_LLEPM_websitelnk= "http://www.apta.com/resources/standards/Documents/APTA-PR-E-S-013-99.pdf"
+export const LLEPM_CFR238115_websitelnk= "http://www.ecfr.gov/cgi-bin/text-idx?SID=4bb7993199b6952b8d7ac5194445e111&mc=true&node=se49.4.238_1115&rgn=div8"
+export const APTA_wire_009_98_websitelnk= "http://www.apta.com/resources/standards/Documents/APTA-PR-E-RP-009-98.pdf"
+export const APTA_wiring_002_98_websitelnk= "http://www.apta.com/resources/standards/Documents/APTA-PR-E-RP-002-98.pdf"
+export const PRIIAAMTRAK_305_003_wireNCable_websitelnk="http://www.highspeed-rail.org/Documents/PRIIA%20305%20DocSpec%20and%20other%20NGEC%20Documents/PRIIA%20Single%20Level%20Spec%20305%20003%20Approved%20Initial%20Release.pdf");
+export const ADA2010Standards="https://www.ada.gov/regs2010/2010ADAStandards/2010ADAStandards_prt.pdf");
+export const NFPA130="http://hamyarenergy.com/static/fckimages/files/NFPA/Hamyar%20Energy%20NFPA%20130%20-%202007.pdf");
+export const CFR_223_appxA="http://www.ecfr.gov/cgi-bin/text-idx?SID=3cd317da8cff8989cb773c31ec93961d&mc=true&node=pt49.4.223&rgn=div5#ap49.4.223_117.a");
+export const CFR_223=("http://www.ecfr.gov/cgi-bin/text-idx?SID=3cd317da8cff8989cb773c31ec93961d&mc=true&node=pt49.4.223&rgn=div5#ap49.4.223_117.a");
+export const EmergencyExit_windows_Figure1="http://www.ecfr.gov/cgi-bin/retrieveECFR?gp=&SID=7e3d57ee30f1e620414221a1465c1255&mc=true&n=sp49.4.238.b&r=SUBPART&ty=HTML#ap49.4.238_1137.1");
+export const Emergencyaccess_windows_Figure1A="http://www.ecfr.gov/cgi-bin/retrieveECFR?gp=&SID=f41884206d216c4454a29ff7ac521e33&mc=true&n=sp49.4.238.b&r=SUBPART&ty=HTML#ap49.4.238_1137.2");
+export const Emergencyaccess_exit_windows_Figure1B="http://www.ecfr.gov/cgi-bin/retrieveECFR?gp=&SID=f41884206d216c4454a29ff7ac521e33&mc=true&n=sp49.4.238.b&r=SUBPART&ty=HTML#ap49.4.238_1137.3");
+export const Emergencyaccess_exit_windows_Figure1C="http://www.ecfr.gov/cgi-bin/retrieveECFR?gp=&SID=f41884206d216c4454a29ff7ac521e33&mc=true&n=sp49.4.238.b&r=SUBPART&ty=HTML#ap49.4.238_1137.4");
+export const CFR_49_238_113="http://www.ecfr.gov/cgi-bin/text-idx?SID=10c4c97aff11ad16cf9a0fff6759a16c&mc=true&node=se49.4.238_1113&rgn=div8");
+export const CFR_49_238_114=("http://www.ecfr.gov/cgi-bin/text-idx?SID=10c4c97aff11ad16cf9a0fff6759a16c&mc=true&node=se49.4.238_1114&rgn=div8");
+export const APTA_egress_access_signage=("http://www.apta.com/resources/standards/Documents/APTA-PR-PS-S-002-98.pdf");
+
+
+****// Supplier website links // ***
+export const KSU_websitelnk = 'http://www.kustomseating.com/"
+export const freedman_websitelnk = "http://freedmanseating.com/"
+export const Compin_websitelnk = "http://www.compin.com/?lang=en"
+export const Kiel_websitelnk= "http://www.kielna.com/\n"
+export const Adtrans_websitelnk= "http://www.adtrans-seating.com/Seating_Systems.htm"
--- src/context/links.js
+++
@@ -1,2 +0,0 @@
-export const link1 = "www.google.com/***testLink"
-export const link2 = "www.google.com/***testLink"

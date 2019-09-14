17e9fe11784024de153e51073589e12ac9576ece
jlcobos <luiscobosj@gmail.com>
2019-09-14 15:46:49 -0700

    more regulation text

---
+++ src/context/RequirementText/InteriorCirculationHandrailsAndStanchions/InteriorCirculationHandrailsAndStanchions.js
@@ -0,0 +1,10 @@
+export default  {
+    pageTitle: "Interior Circulation, handrails and stanchions:",
+    title: "§38.115 Interior circulation, handrails and stanchions.",
+    subTitle: "",
+    paragraphs: [
+        {text: `(a) Where provided, handrails or stanchions within the passenger compartment shall be placed to permit sufficient turning and maneuvering space for wheelchairs and other mobility aids to reach a seating location, complying with §38.125(d) of this part, from an accessible entrance. The diameter or width of the gripping surface of interior handrails and stanchions shall be 11⁄4 inches to 11⁄2 inches or shall provide an equivalent gripping surface. Handrails shall be placed to provide a minimum 11⁄2 inches knuckle clearance from the nearest adjacent surface.`},
+        {text: `(b) Where provided, handrails and stanchions shall be sufficient to permit safe boarding, on-board circulation, seating and standing assistance, and alighting by persons with disabilities.`},
+        {text: `(c) At entrances equipped with steps, handrails or stanchions shall be provided in the entrance to the car in a configuration which allows passengers to grasp such assists from outside the car while starting to board, and to continue using such assists throughout the boarding process, to the extent permitted by part 231 of this title.`},
+    ]
+}
---
+++ src/context/RequirementText/InteriorFittingsAndSurfaces/InteriorFittingsAndSurfaces.js
@@ -0,0 +1,13 @@
+export default  {
+    pageTitle: "Interior Fittings And Surfaces",
+    title: "Interior Fittings And Surfaces",
+    subTitle: "",
+    paragraphs: [
+        {text: `In general all interior fittings and surfaces should meet the following per "CFR 238.233 interior fittings and surfaces":`},
+        {text: `(c) Other interior fittings within a passenger car shall be attached to the car body with sufficient strength to withstand the following individually applied accelerations acting on the mass of the fitting:`},
+        {text: `(1) Longitudinal: 8g;`},        
+        {text: `(2) Vertical: 4g; and`},        
+        {text: `(3) Lateral: 4g.`},        
+        {text: `Additional information can be found under each component found in the main menu.`},               
+    ]
+}

9bbe2632f55b295b24895cca77972d6fcb7e8447
jlcobos <luiscobosj@gmail.com>
2019-09-21 18:05:27 -0700

    .

--- index.js
+++ index.js
@@ -4,16 +4,12 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("../client/build"));
+    app.use(express.static("./client/build"));
+    app.get("/*", function(req, res) {   
+        res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    });
 }
-
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("/*", function(req, res) {   
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
 
 app.listen(PORT, function() {
     console.log(`🌎 ==> Server now on running port ${PORT}!`);

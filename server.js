b73416032b611ea3e10c20b47c7c4883fc3f89f9
jlcobos <luiscobosj@gmail.com>
2019-09-22 21:36:45 -0700

    static file route update

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("./client/build"));
+    app.use(express.static("./build"));
     app.get("/*", function(req, res) {   
-        res.sendFile(path.join(__dirname, "./client/build/index.html"));
+        res.sendFile(path.join(__dirname, "./build/index.html"));
     });
 }
 

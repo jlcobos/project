37eecb0f658935e66244318dbeae659134c7ebb8
jlcobos <luiscobosj@gmail.com>
2019-09-22 22:10:35 -0700

    pushing build

---
+++ build/precache-manifest.ae60be418e967f240c2c287b924a4df2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "addc0149c00c2c49a19221bafe2792ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aded0d59320c0cc4b7d4",
+    "url": "/static/css/2.3ebde4eb.chunk.css"
+  },
+  {
+    "revision": "78a662e38f523528ceab",
+    "url": "/static/css/main.b18c6d27.chunk.css"
+  },
+  {
+    "revision": "aded0d59320c0cc4b7d4",
+    "url": "/static/js/2.5bd22dee.chunk.js"
+  },
+  {
+    "revision": "78a662e38f523528ceab",
+    "url": "/static/js/main.2197234d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

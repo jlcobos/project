6b65d0c7ca652cd44138d382f15342fff13cc7b9
jlcobos <luiscobosj@gmail.com>
2019-09-25 23:11:03 -0700

    page refresh now working

--- server.js
+++ server.js
@@ -7,11 +7,6 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 app.set('trust proxy', true);
 
-app.get("/getjson", function(req, res) {   
-    res.json({test: "OK"});
-});
-
-
 if (process.env.NODE_ENV === "production") {
     app.get("/*", function(req, res) {   
         res.sendFile(path.join(__dirname, 'build', 'index.html'));

a6431e78168efe56a35fe98cda2c40880ddf3a0b
jlcobos <luiscobosj@gmail.com>
2019-11-26 21:52:52 -0800

    fixed issue with package.json

---
+++ build/precache-manifest.0fb0972fb776c8ed92da6e4667bc5c25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1404d7029ddb0c274600bb6e693f651",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b4329e84b0273353c41",
+    "url": "/static/css/2.3ebde4eb.chunk.css"
+  },
+  {
+    "revision": "a0de11a8a65b62b168cc",
+    "url": "/static/css/main.aa612f99.chunk.css"
+  },
+  {
+    "revision": "4b4329e84b0273353c41",
+    "url": "/static/js/2.cd317b8c.chunk.js"
+  },
+  {
+    "revision": "a0de11a8a65b62b168cc",
+    "url": "/static/js/main.75784d28.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c1299420b15fb18eeedb1da575918ba2.js"
+  "/precache-manifest.0fb0972fb776c8ed92da6e4667bc5c25.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
